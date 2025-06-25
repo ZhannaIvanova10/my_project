9705e2b9a8346be9db484044e3dbf147c2d179b4
Жанна Иванова <janett_86@mail.ru>
2025-06-25 20:49:09 +0300

    Перенес тесты processing в корректно названный файл

--- tests/test_processing.py
+++
@@ -1,7 +0,0 @@
-# tests/test_processing.py
-from src.processing import some_function  # импортируйте реальные функции
-import pytest
-
-def test_processing_function():
-    # тестовая логика
-    assert True  # замените на реальные проверки
